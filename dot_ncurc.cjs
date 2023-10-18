87a5ce1839cc7725b367d4f063c91f85ea3df4a2
Sparticuz <kyle@kmcnally.net>
2023-10-18 13:35:35 -0400

    fix update on debian, add ncurc

---
+++ dot_ncurc.cjs
@@ -0,0 +1,35 @@
+/**
+ * This is my ncu config file (npm-check-updates)
+ */
+module.exports = {
+  interactive: true,
+  format: "group,repo",
+  /**
+   *
+   * @param name             The name of the dependency.
+   * @param defaultGroup     The predefined group name which will be used by default.
+   * @param currentSpec      The current version range in your package.json.
+   * @param upgradedSpec     The upgraded version range that will be written to your package.json.
+   * @param upgradedVersion  The upgraded version number returned by the registry.
+   * @returns                A predefined group name ('major' | 'minor' | 'patch' | 'majorVersionZero' | 'none') or a custom string to create your own group.
+   */
+  groupFunction: (
+    name,
+    defaultGroup,
+    currentSpec,
+    upgradedSpec,
+    upgradedVersion
+  ) => {
+    if (name.startsWith("@dazser/")) {
+      return "DAZSER";
+    }
+    return defaultGroup;
+  },
+  /**
+   * I don't want to do any major upgrade's \@types/node unless I move node versions
+   * @param name The name of the dependency.
+   * @param parsedVersion A parsed Semver object of the upgraded version.
+   * (See: https://git.coolaj86.com/coolaj86/semver-utils.js#semverutils-parse-semverstring)
+   */
+  target: (name) => (name === "@types/node" ? "minor" : "latest"),
+};
